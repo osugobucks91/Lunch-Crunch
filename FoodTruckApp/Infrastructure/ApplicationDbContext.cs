bc2aa83679afe06544cb90760de606aeede1bd67
Tyler Hugus <tyler.hugus@gmail.com>
2015-12-18 16:02:52 -0600

    new template

---
+++ FoodTruckApp/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FoodTruckApp.Domain.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodTruckApp.Infrastructure {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false) {
+        }
+
+        public static ApplicationDbContext Create() {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ FoodTruckApp/Presentation/ngApp/app.ts
@@ -0,0 +1,9 @@
+﻿namespace FoodTruckApp {
+    angular.module("FoodTruckApp", ['ngRoute'])
+        .config(function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    template: '<p>Hello World</p>'
+                });
+        });
+}
