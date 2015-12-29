271825a2e2d26e399c91a8461aeb2dcc045347ee
Mike Cline <mike_cline@outlook.com>
2015-12-29 15:56:30 -0600

    menu item ts model in ngApp

--- FoodTruckApp/Presentation/ngApp/models/menuItem.ts
+++ FoodTruckApp/Presentation/ngApp/models/menuItem.ts
@@ -1,1 +1,12 @@
-﻿
+﻿namespace FoodTruckApp.Models {
+
+    export class MenuItem {
+
+        public id: string;
+        public name: string;
+        public price: number;
+        public description: string;
+        public image: string;      //should be optional
+        public owner;
+    }
+}
