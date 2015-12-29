f16788dcbbfbaa0913b7aeff8b4ae7ecb3896d09
Tyler Hugus <tyler.hugus@gmail.com>
2015-12-29 16:26:16 -0600

    food truck service

---
+++ FoodTruckApp/Services/FoodTruckService.cs
@@ -0,0 +1,61 @@
+﻿using FoodTruckApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace FoodTruckApp.Services {
+    class FoodTruckService {
+
+        private FoodTruckRepository _repo;
+
+        public FoodTruckService(FoodTruckRepository repo) {
+            _repo = repo;
+        }
+
+        private FoodTruckDTO Map(FoodTruck dbFoodTruck) {
+            return dbFoodTruck != null ? new FoodTruckDTO() {
+                Id = dbFoodTruck.Id,
+                Name = dbFoodTruck.Name,
+                Image = dbFoodTruck.Image,
+                Description = dbFoodTruck.Description,
+                Location = dbFoodTruck.Location,
+                BusinessOwner = new BusinessUserDTO() {
+                    Id = dbFoodTruck.BusinessOwner.Id,
+                    UserName = dbFoodTruck.BusinessOwner.UserName
+                },
+                MenuItems = (from m in dbFoodTruck.MenuItems
+                             select new MenuItemDTO() {
+                                 Id = m.Id,
+                                 Name = m.Name,
+                                 Price = m.Price,
+                                 Description = m.Description,
+                                 ImageUrl = m.ImageUrl
+                             }).ToList(),
+                Reviews = (from r in dbFoodTruck.Reviews
+                           select new ReviewDTO() {
+                               Id = r.Id,
+                               StarRating = r.StarRating,
+                               Message = r.Message
+                           }).ToList()
+            } : null;
+        }
+
+        private FoodTruck Map(FoodTruckDTO dtoFoodTruck) {
+            return dtoFoodTruck != null ? new FoodTruck() {
+                Id = dtoFoodTruck.Id,
+                Name = dtoFoodTruck.Name,
+                Image = dtoFoodTruck.Image,
+                Description = dtoFoodTruck.Description
+            } : null;
+        }
+
+        [HttpPost]
+        public AddNewFoodTruck(FoodTruckDTO newFoodTruck) {
+            _repo.Add(Map(newFoodTruck));
+            _repo.SaveChanges();
+        }
+    }
+}
