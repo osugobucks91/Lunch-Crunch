f89b9d19e88a6ccd5a8478160d4c3e0585222358
Rushan Sakhibgareev <sakhibgareevrr@gmail.com>
2015-12-29 14:00:42 -0600

    test

---
+++ FoodTruckApp/Presentation/ngApp/controllers/BusinessLoginController.ts
@@ -0,0 +1,33 @@
+﻿namespace FoodTruckApp.Controllers {
+    
+    export class BusinessLoginController {
+
+        public username: string;
+
+        public password: string;
+
+        public loginMessage: string;
+
+        constructor(private $http, private $window, private $location) { }
+
+        public businessLogin() {
+
+            let data = `grant_type=password&username=${this.username}&password=${this.password}`;
+
+            this.$http.post('/token', data, {
+                headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+            })
+                .then((response) => {
+                    this.$window.localStorage.setItem('token', response.data['access_token']);
+                    this.$location.path('/business');
+                })
+                .catch((response) => {
+                    this.loginMessage = 'invalid username or password';
+                });
+        }
+
+    }
+    angular.module("FoodTruckApp").controller("businessAuthController", BusinessLoginController);
+
+    }
+
