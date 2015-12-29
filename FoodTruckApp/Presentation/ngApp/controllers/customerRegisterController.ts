fcaf946eeb4bea66b91b5a11ce071535d835abb7
Tyler Hugus <tyler.hugus@gmail.com>
2015-12-29 13:52:11 -0600

    ngController for Customer Register

---
+++ FoodTruckApp/Presentation/ngApp/controllers/customerRegisterController.ts
@@ -0,0 +1,32 @@
+﻿namespace FoodTruckApp.Controllers {
+
+    import Customer = FoodTruckApp.Models.CustomerUser;
+
+    export class RegisterController {
+
+        public newCustomer: Customer;
+        public validationErrors;
+
+        constructor(private $http, private $window, private $location) { }
+
+        public register() {
+
+            this.$http.post('/api/account/register', this.newCustomer)
+                .then((response) => {
+                    this.$location.path('/customer');
+                    this.newCustomer = null;
+                })
+                .catch((response) => {
+                    this.validationErrors = [];
+                    let modelState = response.data.modelState;
+                    for (let error in modelState) {
+                        this.validationErrors = this.validationErrors.concat(modelState[error]);
+                    }
+                })
+        }
+
+        public goToLogin() {
+            this.$location.path('/customer/login');
+        }
+    }
+}
