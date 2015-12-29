e70dfd81ea6aa15b52542f515d587ab8a0457690
Tyler Hugus <tyler.hugus@gmail.com>
2015-12-29 16:08:51 -0600

    Menu Item DTO

--- FoodTruckApp/Services/MenuItemDTO.cs
+++ FoodTruckApp/Services/MenuItemDTO.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace FoodTruckApp.Domain.Models {
-    public class MenuItem {
+namespace FoodTruckApp.Services {
+    class MenuItemDTO {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
-        public string ImageUrl { get; set; }                 //Optional Image Upload
-        public ApplicationUser BusinessOwner { get; set; }
-        
+        public string ImageUrl { get; set; } 
+        public BusinessUserDTO BusinessOwner { get; set; }
     }
 }
