653d65a612e1a37705adba9f9c996f3f3b0900b7
Rushan Sakhibgareev <sakhibgareevrr@gmail.com>
2015-12-30 15:13:43 -0600

    review repository for review model has been edited

--- FoodTruckApp/Infrastructure/Repository/ReviewRepository.cs
+++ FoodTruckApp/Infrastructure/Repository/ReviewRepository.cs
@@ -1,18 +1,22 @@
-﻿using System;
+﻿using CoderCamps.Infrastructure.Repository;
+using FoodTruckApp.Domain.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace FoodTruckApp.Infrastructure.Repository
 {
-    public class ReviewRepository
+    public class ReviewRepository: GenericRepository<Review>
     {
         public ReviewRepository(DbContext db) : base(db) { }
 
-        protected override IQuarable<Review> Include(IQueryable<> )
-
-
-
+        protected override IQueryable<Review> Include(IQueryable<Review> query)
+        {
+            return query.Include(r => r.CustomerUsername)
+                        .Include(r => r.TruckId);
+        }
 
     }
 }
