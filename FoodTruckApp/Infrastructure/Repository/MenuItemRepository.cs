d73a5b53e6b65a2c5891661227dbf7e40f5b35f0
Rushan Sakhibgareev <sakhibgareevrr@gmail.com>
2015-12-31 09:14:53 -0600

    edited html page with business html page with added location input field and MenuItem Repository file to seed the database

---
+++ FoodTruckApp/Infrastructure/Repository/MenuItemRepository.cs
@@ -0,0 +1,29 @@
+﻿using CoderCamps.Infrastructure.Repository;
+using FoodTruckApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FoodTruckApp.Infrastructure.Repository
+{
+    public class MenuItemRepository : GenericRepository<MenuItem>
+    {
+        public MenuItemRepository(ApplicationDbContext db) : base(db) { }
+
+        protected override IQueryable<MenuItem> Include(IQueryable<MenuItem> query)
+        {
+            return query.Include(m => m.BusinessOwner)
+                        .Include(m => m.FoodTruck);
+        }
+
+        public MenuItem FindById(int id, string username)
+        {
+            return (from m in Table
+                    where m.Id == id && m.BusinessOwner.UserName == username
+                    select m).FirstOrDefault();
+        }
+
+    }
+}
