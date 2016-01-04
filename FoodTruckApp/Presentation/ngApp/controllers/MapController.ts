2a07459f8f7a2814d9c01d61bdd48790658d50df
Rushan Sakhibgareev <sakhibgareevrr@gmail.com>
2016-01-04 13:22:41 -0600

    modified list and map html files plus made goToList in MapController file

--- FoodTruckApp/Presentation/ngApp/controllers/MapController.ts
+++ FoodTruckApp/Presentation/ngApp/controllers/MapController.ts
@@ -1,6 +1,15 @@
 ﻿namespace FoodTruckApp.Controllers {
 
     export class MapController {
+        constructor(private $http, private $location, private $routeParams) {
+
+            this.$location = $location;
+
+        }
+        
+        public goToList() {
+            this.$location.path('/list');
+        }
 
     }
 }
