5155d786e51a33a83156a5b3f4e0caf868728197
Tyler Hugus <tyler.hugus@gmail.com>
2016-01-04 11:12:15 -0600

    app.ts routing and ngController for food truck list view

---
+++ FoodTruckApp/Presentation/ngApp/controllers/FoodTruckListController.ts
@@ -0,0 +1,23 @@
+﻿namespace FoodTruckApp.Controllers {
+
+    import FoodTruck = FoodTruckApp.Models.FoodTruck;
+
+    export class FoodTruckListController {
+
+        public foodTrucks: FoodTruck[];
+
+        constructor(private $http, private $location, private $routeParams) {
+
+            this.$location = $location;
+
+            $http.get('api/foodtrucks')
+                .then((response) => {
+                    this.foodTrucks = response.data;
+                })
+        }
+
+        public goToMap() {
+            this.$location.path('/map');
+        }
+    }
+}
